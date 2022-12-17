f86b0c08dfc95c4530a8601f6dacb146569799ff
nelson <ludonelvic@gmail.com>
2022-12-18 00:53:10 +0100

    this is the last c code for today

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  *main - this is to show the sizes
  *
- *return: always 0(success)
+ *Return: always 0(success)
  */
 
 int main(void)
@@ -14,12 +14,11 @@ char c;
 long long int l;
 long int li;
 
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l));
-printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
 
