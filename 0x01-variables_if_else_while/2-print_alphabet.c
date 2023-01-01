148af344ee296f4e7743f168155198a372f98dc8
nelson <ludonelvic@gmail.com>
2023-01-01 17:04:45 +0100

    this is the second exercise

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
- *
  */
 
 int main(void)
 {
 	char n;
+
 	for (n = 'a'; n <= 'z'; n++)
 	{
 		putchar(n);
