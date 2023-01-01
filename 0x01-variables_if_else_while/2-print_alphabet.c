fe5d096c2300417779a97f6013c58650c9f194f9
nelson <ludonelvic@gmail.com>
2023-01-01 16:41:57 +0100

    this is the second exercise

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		putchar(n);
+		putchar('\n');
+	return (0);
+}
