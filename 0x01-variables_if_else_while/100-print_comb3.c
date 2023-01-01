60c4b8de838783e249133065077558a16f809987
nelson <ludonelvic@gmail.com>
2023-01-01 18:34:58 +0100

    this is the 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,18 +9,19 @@
 int main(void)
 {
 	int n;
+	int m;
 
-	for (n = '0'; n <= '89'; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		if (n <= '9')
-			putchar('0');
-			putchar(n);
-		putchar(n);
-		if (n == '89')
-			break;
 
-		putchar(',');
-		putchar(' ');
+		for (m = '1'; m <= '9'; m++)
+			putchar(n);
+			putchar(m);
+			if (m == '9')
+				break;
+
+			putchar(',');
+			putchar(' ');
 	}
 
 	putchar('\n');
