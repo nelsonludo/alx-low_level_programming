f9ca3e4dfdf6d731dc30cdfe0d98a6a233a8c9bc
nelson <ludonelvic@gmail.com>
2023-01-02 22:52:16 +0100

    this is the seventh exercise

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit- Check Description
+ * @n: An input number
+ * Description: function uses _putchar function to print
+ * Return: the last digit of a number
+ */
+
+int print_last_digit(int n)
+{
+	int last = n % 10;
+
+	return (last);
+}
