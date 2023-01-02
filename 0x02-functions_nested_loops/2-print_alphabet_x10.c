957ba62a07184bb6bc71e3e6bd9fabf25e05337b
nelson <ludonelvic@gmail.com>
2023-01-02 20:16:49 +0100

    this might be the solution to the fist task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Nothing.
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int n = 1;
+
+	while (n <= 10) 
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	n++;
+	}
+}
